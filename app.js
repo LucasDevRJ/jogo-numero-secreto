f65c1323ab083f91921ce0bdf05ddc23ff091768
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-11-02 20:40:41 -0300

    Condições para número digitado ser maior ou menor.

--- app.js
+++ app.js
@@ -3,11 +3,15 @@ let numeroSecreto = 7;
 let numeroDigitado = prompt("Digite um número entre 0 e 10: ");
 
 console.log(numeroSecreto);
-console.log(`O número digitado é igual ao número sorteado: ${numeroSecreto == numeroDigitado}`);
+console.log(`O número digitado é igual ao número secreto: ${numeroSecreto == numeroDigitado}`);
 
-//se o número digitado for igual o número sorteado
+//se o número digitado for igual o número secreto
 if (numeroSecreto == numeroDigitado) {
     alert(`Acertou!! O número secreto é ${numeroSecreto}`);
 } else {
-    alert(`Errou!! O número secreto é ${numeroSecreto}`);
+    if (numeroSecreto > numeroDigitado) {
+        alert(`O número secreto é maior que ${numeroDigitado}`);
+    } else  {
+        alert(`O número secreto é menor que ${numeroDigitado}`);
+    }
 }
