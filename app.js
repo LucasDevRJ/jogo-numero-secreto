53a7649fc773bb9b6aeae3f03cb3fd93e782ac73
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-11-02 14:10:42 -0300

    Adição de condicional.

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 alert("Bem-vindo ao jogo do número secreto!");
 let numeroSecreto = 7;
-prompt("Digite um número entre 0 e 10: ");
+let numeroDigitado = prompt("Digite um número entre 0 e 10: ");
+
+if (numeroSecreto == numeroDigitado) {
+    console.log("Acertou!! Você acertou o número secreto que é " + numeroSecreto);
+}
