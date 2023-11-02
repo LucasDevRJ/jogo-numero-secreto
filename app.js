de1c5368fdaa6ce2865e90457ebfd5750fb7453b
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-11-02 20:52:36 -0300

    Adição da estrutura de repetição.

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
 alert("Bem-vindo ao jogo do número secreto!");
 let numeroSecreto = 7;
-let numeroDigitado = prompt("Digite um número entre 0 e 10: ");
+let numeroDigitado;
 
 console.log(numeroSecreto);
 console.log(`O número digitado é igual ao número secreto: ${numeroSecreto == numeroDigitado}`);
 
-//se o número digitado for igual o número secreto
-if (numeroSecreto == numeroDigitado) {
-    alert(`Acertou!! O número secreto é ${numeroSecreto}`);
-} else {
-    if (numeroSecreto > numeroDigitado) {
-        alert(`O número secreto é maior que ${numeroDigitado}`);
-    } else  {
-        alert(`O número secreto é menor que ${numeroDigitado}`);
+//enquanto o número secreto não for igual ao número digitado
+while (numeroSecreto != numeroDigitado) {
+    numeroDigitado = prompt("Digite um número entre 0 e 10: ");
+
+    //se o número digitado for igual o número secreto
+    if (numeroSecreto == numeroDigitado) {
+        alert(`Acertou!! O número secreto é ${numeroSecreto}`);
+    } else {
+        if (numeroSecreto > numeroDigitado) {
+            alert(`O número secreto é maior que ${numeroDigitado}`);
+        } else  {
+            alert(`O número secreto é menor que ${numeroDigitado}`);
+        }
     }
 }
