2081df6ff843d6ee8b4a2f61ff1d138c3a432b1e
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-11-02 19:58:25 -0300

    Adição de console.log no programa.

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ alert("Bem-vindo ao jogo do número secreto!");
 let numeroSecreto = 7;
 let numeroDigitado = prompt("Digite um número entre 0 e 10: ");
 
+console.log(numeroSecreto);
+console.log(`O número digitado é igual ao número sorteado: ${numeroSecreto == numeroDigitado}`);
+
 //se o número digitado for igual o número sorteado
 if (numeroSecreto == numeroDigitado) {
     alert(`Acertou!! O número secreto é ${numeroSecreto}`);
