64034b6eb12174d8a5ba4a7ccad33368872fdaac
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-11-02 14:56:13 -0300

    Dupla condição.

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ alert("Bem-vindo ao jogo do número secreto!");
 let numeroSecreto = 7;
 let numeroDigitado = prompt("Digite um número entre 0 e 10: ");
 
+//se o número digitado for igual o número sorteado
 if (numeroSecreto == numeroDigitado) {
-    console.log("Acertou!! Você acertou o número secreto que é " + numeroSecreto);
+    alert("Acertou!! O número secreto é " + numeroSecreto);
+} else {
+    alert("Errou!! O número secreto é " + numeroSecreto);
 }
