048e07807e17a4685f52bfc9770986a88a163366
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-11-03 19:30:13 -0300

    Adição de operador ternário

--- app.js
+++ app.js
@@ -1,26 +1,29 @@
 alert("Bem-vindo ao jogo do número secreto!");
 let numeroSecreto = 7;
 let numeroDigitado;
-let tentativas = 3;
+let tentativas = 0;
 
 console.log(numeroSecreto);
 console.log(`O número digitado é igual ao número secreto: ${numeroSecreto == numeroDigitado}`);
 
 //enquanto o número secreto não for igual ao número digitado
-while (tentativas > 0) {
+while (tentativas >= 0) {
     numeroDigitado = prompt("Digite um número entre 0 e 10: ");
 
+    tentativas++;
     //se o número digitado for igual o número secreto
     if (numeroSecreto == numeroDigitado) {
-        alert(`Acertou!! O número secreto é ${numeroSecreto}`);
+        alert(`Acertou!! Você acertou o número secreto que é ${numeroSecreto}.`);
         break;
     } else {
         if (numeroSecreto > numeroDigitado) {
-            alert(`O número secreto é maior que ${numeroDigitado}`);
+            alert(`Errou!! O número secreto é maior que ${numeroDigitado}.`);
         } else  {
-            alert(`O número secreto é menor que ${numeroDigitado}`);
+            alert(`Errou!! O número secreto é menor que ${numeroDigitado}.`);
         }
     }
-    tentativas--;
-    alert(`O número de tentativas restantes é ${tentativas}`);
 }
+
+let foramMaisDeUmaTentativa = tentativas > 1 ? "tentativas" : "tentativa";
+
+alert(`Você descobriu o número secreto com ${tentativas} ${foramMaisDeUmaTentativa}.`);
