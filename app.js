a3b1ab12f962bcced3dc94ddc6caaa09ae8ec88d
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-11-02 21:35:05 -0300

    Número de tentativas para acertar o número.

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 alert("Bem-vindo ao jogo do número secreto!");
 let numeroSecreto = 7;
 let numeroDigitado;
+let tentativas = 3;
 
 console.log(numeroSecreto);
 console.log(`O número digitado é igual ao número secreto: ${numeroSecreto == numeroDigitado}`);
 
 //enquanto o número secreto não for igual ao número digitado
-while (numeroSecreto != numeroDigitado) {
+while (tentativas > 0) {
     numeroDigitado = prompt("Digite um número entre 0 e 10: ");
 
     //se o número digitado for igual o número secreto
     if (numeroSecreto == numeroDigitado) {
         alert(`Acertou!! O número secreto é ${numeroSecreto}`);
+        break;
     } else {
         if (numeroSecreto > numeroDigitado) {
             alert(`O número secreto é maior que ${numeroDigitado}`);
@@ -19,4 +21,6 @@ while (numeroSecreto != numeroDigitado) {
             alert(`O número secreto é menor que ${numeroDigitado}`);
         }
     }
+    tentativas--;
+    alert(`O número de tentativas restantes é ${tentativas}`);
 }
