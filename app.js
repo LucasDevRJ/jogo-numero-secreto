efd3eeb2e13b1a428d3c824a283f5ec4eeb593a3
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-11-04 15:37:40 -0300

    Manipulando elementos HTML com JS

--- app.js
+++ app.js
@@ -1,29 +1,5 @@
-/*
-alert("Bem-vindo ao jogo do número secreto!");
-let numeroLimite = 30;
-let numeroSecreto = Math.round(Math.random() * numeroLimite);
-let numeroDigitado;
-let tentativas = 0;
-console.log(`O número digitado é igual ao número secreto: ${numeroSecreto == numeroDigitado}`);
+let titulo = document.querySelector("h1");
+titulo.innerHTML = "Jogo do Número Secreto";
 
-//enquanto o número secreto não for igual ao número digitado
-while (tentativas >= 0) {
-    numeroDigitado = prompt(`Digite um número entre 0 e ${numeroLimite}: `);
-
-    tentativas++;
-    //se o número digitado for igual o número secreto
-    if (numeroSecreto == numeroDigitado) {
-        alert(`Acertou!! Você acertou o número secreto que é ${numeroSecreto}.`);
-        break;
-    } else {
-        if (numeroSecreto > numeroDigitado) {
-            alert(`Errou!! O número secreto é maior que ${numeroDigitado}.`);
-        } else  {
-            alert(`Errou!! O número secreto é menor que ${numeroDigitado}.`);
-        }
-    }
-}
-
-let foramMaisDeUmaTentativa = tentativas > 1 ? "tentativas" : "tentativa";
-
-alert(`Você descobriu o número secreto com ${tentativas} ${foramMaisDeUmaTentativa}.`);
+let paragrafo = document.querySelector("p");
+paragrafo.innerHTML = "Escolha um número entre 1 e 10:";
